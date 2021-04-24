27d10295190d007574c7e1ad594bfabae83737d8
Luan Chagas <45405945+LuanChagas@users.noreply.github.com>
2021-04-24 19:13:51 -0300

    Bug Fix

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,37 +1,30 @@
-import calculation from './doingCalculation.js';
+import calculation from "./doingCalculation.js";
 
-let calc = new calculation;
-
+let calc = new calculation();
 
 export default class Calculator {
+  constructor() {
+    this.btn = document.querySelectorAll(".btn");
+    this.toMakeCaculation();
+  }
 
-    constructor() {
-        this.btn = document.querySelectorAll('.btn')
-        this.toMakeCaculation()
-    }
+  addEffectBtn(bt) {
+    bt.classList.add("btnClicked");
+  }
 
+  removeEffectBtn(bt) {
+    bt.classList.remove("btnClicked");
+  }
 
-
-    addEffectBtn(bt) {
-        bt.classList.add('btnClicked')
-    }
-
-    removeEffectBtn(bt) {
-        bt.classList.remove('btnClicked')
-    }
-
-
-    toMakeCaculation() {
-
-        this.btn.forEach(bt => {
-            bt.addEventListener('click', (e) => {
-                this.addEffectBtn(bt)
-                calc.choice(bt.id)
-                setTimeout(() => {
-                    this.removeEffectBtn(bt)
-                }, 100)
-            });
-
-        })
-    }
+  toMakeCaculation() {
+    this.btn.forEach((bt) => {
+      bt.addEventListener("click", (e) => {
+        this.addEffectBtn(bt);
+        calc.choice(bt.id);
+        setTimeout(() => {
+          this.removeEffectBtn(bt);
+        }, 100);
+      });
+    });
+  }
 }
--- js/modules/doingCalculation.js
+++ js/modules/doingCalculation.js
@@ -1,167 +1,148 @@
 export default class DoCalculation {
-    constructor() {
-        this.display = document.querySelector('.display-numbers span')
+  constructor() {
+    this.display = document.querySelector(".display-numbers span");
 
-        this.oper = ["/", "*", "-", "+"]
-        this.numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
-        this.clean = "C"
-        this.perc = "%"
+    this.oper = ["/", "*", "-", "+"];
+    this.numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+    this.clean = "C";
+    this.perc = "%";
 
-        this.equalsCalc = "="
-        this.ce = "CE"
+    this.equalsCalc = "=";
+    this.ce = "CE";
 
-        this.show = []
-        this.cal = []
+    this.show = [];
+    this.cal = [];
+  }
+
+  showScreen() {
+    if (!this.show.length) {
+      this.display.innerHTML = 0;
+    } else {
+      this.display.innerHTML = this.show.join("");
     }
+  }
 
-    showScreen() {
+  clearScreen() {
+    this.display.innerHTML = 0;
+    this.show = [];
+    this.cal = [];
+  }
 
-        if (!this.show.length) {
-            this.display.innerHTML = 0
-        } else {
-            this.display.innerHTML = this.show.join('')
-        }
+  equalsOper() {
+    let hasOper = false;
+
+    this.cal.forEach((x) => {
+      if (this.oper.includes(x)) {
+        hasOper = true;
+      }
+    });
+    return hasOper;
+  }
+
+  lastIsOper() {
+    let isOper = false;
+
+    this.oper.forEach((op) => {
+      if (this.cal[this.cal.length - 1] === op) {
+        isOper = true;
+      }
+    });
+    console.log(isOper);
+    return isOper;
+  }
+
+  firstIsOper() {
+    let isOper = false;
+
+    if (this.cal[0] === "-") {
+      isOper = true;
     }
+    return isOper;
+  }
 
-    clearScreen() {
-        this.display.innerHTML = 0
-        this.show = []
-        this.cal = []
+  operator(value) {
+    if (this.lastIsOper()) {
+      if (this.firstIsOper() && value === "+") {
+        this.show.pop();
+      } else if (this.show.length > 1) {
+        this.show[this.show.length - 1] = value;
+        this.showScreen();
+      }
+    } else if (this.equalsOper()) {
+      this.result();
+      this.show.push(value);
+      this.cal.push(value);
+    } else if (this.cal[0]) {
+      this.cal.push(value);
+      this.show.push(value);
+      this.showScreen();
+    } else if (!this.cal[0] && value === "-") {
+      this.show.push(value);
+      this.cal.push(value);
     }
+    console.log("show " + this.show);
+    console.log("cal " + this.cal);
+  }
 
-    equalsOper() {
-        let hasOper = false
-
-        this.cal.forEach(x => {
-            if (this.oper.includes(x)) {
-                hasOper = true
-            }
-        })
-        return hasOper
-
+  numbersChoice(value) {
+    let ver = this.show.join("");
+    if (value) {
+      if (ver.length <= 9) {
+          if(value === '.' && this.display.innerHTML.includes('.')){
+            this.showScreen();
+      }else {
+        this.show.push(value);
+        this.cal.push(value);
+        this.showScreen();
+      }
     }
+    console.log(this.display.innerHTML);
+  }
+}
 
-    lastIsOper() {
-        let isOper = false
-
-        this.oper.forEach(op => {
-            if (this.cal[this.cal.length - 1] === op) {
-                isOper = true
-            }
-        })
-        console.log(isOper)
-        return isOper
+  doPercent() {
+    if (!this.lastIsOper()) {
+      if (this.display.innerHTML == 0) {
+        this.display.innerHTML = 0;
+      } else {
+        let res = eval(this.show.join(""));
+        this.result();
+        res = this.display.innerHTML = res / 100;
+        this.show = [res];
+      }
     }
+  }
 
-    firstIsOper() {
-        let isOper = false
+  cleanRegis() {
+    this.cal.pop();
+    this.show.pop();
+    this.showScreen();
+  }
 
-        if (this.cal[0] === '-') {
-            isOper = true
-        }
-        return isOper
-
+  result() {
+    let resul = eval(this.show.join(""));
+    console.log(resul);
+    if (resul != undefined) {
+      this.cal = [];
+      this.cal.push(resul);
+      this.display.innerHTML = resul;
+      this.show = [resul];
     }
+  }
 
-    operator(value) {
-        if (this.lastIsOper()) {
-            if (this.firstIsOper() && value === "+") {
-                this.show.pop()
-            }
-
-            else if (this.show.length > 1) {
-                this.show[this.show.length - 1] = value
-                this.showScreen()
-                console.log("aqui")
-            }
-
-        } else if (this.equalsOper()) {
-            console.log('aqui')
-            this.result()
-            this.show.push(value)
-            this.cal.push(value)
-            console.log('show ' + this.show)
-            console.log('cal ' + this.cal)
-        } else if (this.cal[0]) {
-            this.cal.push(value)
-            this.show.push(value)
-            this.showScreen()
-        } else if (!this.cal[0] && value === '-') {
-            this.show.push(value)
-            this.cal.push(value)
-
-        }
-        console.log('show ' + this.show)
-        console.log('cal ' + this.cal)
+  choice(value) {
+    if (this.numbers.includes(value)) {
+      this.numbersChoice(value);
+    } else if (this.clean === value) {
+      this.clearScreen();
+    } else if (this.oper.includes(value)) {
+      this.operator(value);
+    } else if (value === this.equalsCalc) {
+      this.result();
+    } else if (value === this.ce) {
+      this.cleanRegis();
+    } else if (value === this.perc) {
+      this.doPercent(value);
     }
-
-    numbersChoice(value) {
-        let ver = this.show.join('')
-        console.log(ver)
-      
-        if (value) {
-            console.log("aqui")
-            if(ver.length <= 9){
-                this.show.push(value)
-                this.cal.push(value)
-                this.showScreen()
-                console.log(this.cal)
-            }
-            
-        }
-        
-    }
-
-    doPercent() {
-
-        if (!this.lastIsOper()) {
-            let res = eval(this.show.join(''))
-            this.result()
-            res =
-                this.display.innerHTML = res / 100
-            this.show = [res]
-        }
-    }
-
-
-    cleanRegis() {
-        this.cal.pop()
-        this.show.pop()
-        this.showScreen()
-    }
-
-    result() {
-        let resul = eval(this.show.join(''))
-        console.log(resul)
-        if (resul != undefined) {
-            this.cal = []
-            this.cal.push(resul)
-            console.log("aqui resultado")
-
-            this.display.innerHTML = resul
-            this.show = [resul]
-        }
-    }
-
-
-    choice(value) {
-
-
-     
-        if (this.numbers.includes(value)) { 
-            this.numbersChoice(value)
-        } else if (this.clean === value) {
-            this.clearScreen()
-        } else if (this.oper.includes(value)) {
-            this.operator(value)
-        } else if (value === this.equalsCalc) {
-            this.result()
-        } else if (value === this.ce) {
-            this.cleanRegis()
-        } else if (value === this.perc) {
-            this.doPercent(value)
-        }
-    }
-
-
+  }
 }
